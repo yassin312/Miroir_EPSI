986792dc945674f8db9c87a9590b7128993f0b2d
mgaisnon <mathieu.gaisnon@gmail.com>
2025-06-11 12:07:02 +0200

    Refacto arrange act assert

--- tests/test.py
+++ tests/test.py
@@ -4,33 +4,60 @@ from app import main
 
 class TestMain(unittest.TestCase):
 
+    # ETANT DONNE
+    # QUAND il est 8h
+    # ALORS on obtient "Bonjour"
     def test_salutation_matin(self):
-        # ETANT DONNE 
-        # QUAND il est 8h
-        # ALORS on obtient "Bonjour"
-        self.assertEqual(main.salutation_selon_heure(8), "Bonjour")
+        # Arrange
+        heure = 8
 
+        # Act
+        resultat = main.salutation_selon_heure(heure)
+
+        # Assert
+        self.assertEqual(resultat, "Bonjour")
+
+    # ETANT DONNE
+    # QUAND il est 23h
+    # ALORS on obtient "Bonsoir"
     def test_salutation_soir(self):
-        # ETANT DONNE 
-        # QUAND il est 23h
-        # ALORS on obtient "Bonsoir"
-        self.assertEqual(main.salutation_selon_heure(23), "Bonsoir")
+        # Arrange
+        heure = 23
 
+        # Act
+        resultat = main.salutation_selon_heure(heure)
+
+        # Assert
+        self.assertEqual(resultat, "Bonsoir")
+
+    # ETANT DONNE
+    # QUAND le mot est kayak
+    # ALORS on obtient le même mot good job
     def test_analyse_palindrome(self):
-        # ETANT DONNE 
-        # QUAND le mot est kayak
-        # ALORS on obtient le même mot good job
-        mot_inverse, est_palindrome = main.analyser_texte("kayak")
+        # Arrange
+        mot = "kayak"
+
+        # Act
+        mot_inverse, est_palindrome = main.analyser_texte(mot)
+
+        # Assert
         self.assertEqual(mot_inverse, "kayak")
         self.assertTrue(est_palindrome)
 
+    # ETANT DONNE
+    # QUAND le mot est mathieu
+    # ALORS on obtient le mot à l'envers et Essaye encore
     def test_analyse_non_palindrome(self):
-        # ETANT DONNE 
-        # QUAND le mot est mathieu
-        # ALORS on obtient le mot à l'envers et Essaye encore
-        mot_inverse, est_palindrome = main.analyser_texte("mathieu")
+        # Arrange
+        mot = "mathieu"
+
+        # Act
+        mot_inverse, est_palindrome = main.analyser_texte(mot)
+
+        # Assert
         self.assertEqual(mot_inverse, "ueihtam")
         self.assertFalse(est_palindrome)
+
 
 if __name__ == '__main__':
     unittest.main()
