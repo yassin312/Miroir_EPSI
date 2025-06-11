c78f5ba721bca3d6d181d1a7989d3690bc2e0e2f
mgaisnon <mathieu.gaisnon@gmail.com>
2025-06-11 09:31:17 +0200

    ajout lang

--- app/main.py
+++ app/main.py
@@ -1,25 +1,30 @@
 from datetime import datetime
+import locale
+
+lang, encoding = locale.getlocale()
 
 temps = datetime.now()
 
-if ( temps.hour > 16) :
-    print("Bonsoir")
-else:
-    print("Bonjour")
+if(lang == "fr_FR"):
 
-string = ""
-mot_inverse = ""
-while (string !="exit"):
-
-    string = input("Entrez votre phrase : ")
-    print(string)
-
-    mot_inverse = string[::-1]
-    print("mot inversé : ",mot_inverse )
-
-    if (string == mot_inverse) :
-        print("Good job")
+    if ( temps.hour > 16) :
+        print("Bonsoir")
     else:
-        print("Essaye encore")
+        print("Bonjour")
 
-print("AU revoir")
+    string = ""
+    mot_inverse = ""
+    while (string !="exit"):
+
+        string = input("Entrez votre phrase : ")
+        print(string)
+
+        mot_inverse = string[::-1]
+        print("mot inversé : ",mot_inverse )
+
+        if (string == mot_inverse) :
+            print("Good job")
+        else:
+            print("Essaye encore")
+
+    print("AU revoir")
